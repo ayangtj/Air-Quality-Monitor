7c85b4b3e8d8d478e6539356fc7eef51c68afa36
ayang <aprilyang12@gmail.com>
2020-06-12 13:28:00 -0700

    current_mode
    displaying current mode in tkinter GUI

---
+++ tkinter_current_mode.py
@@ -0,0 +1,14 @@
+import tkinter 
+#from simulator import current_quality
+
+
+window = tkinter.Tk()
+
+window.title('Air Quality Current Mode')
+
+
+
+label = tkinter.Label(window, text = 'Current Air Quality: Moderate').pack()
+
+
+window.mainloop()
