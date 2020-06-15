347d70fba6cf01fa06a9808f127e4595a4aff40c
ayang <aprilyang12@gmail.com>
2020-06-14 18:37:19 -0700

    TODO
    Todo list to keep track

---
+++ todo.py
@@ -0,0 +1,11 @@
+todo_list = {
+    'current_mode_file1': 'debug_consistent_datatype',
+    'current_mode_file2': 'average_time_and_reading',
+    'current_mode_file3': 'display_on_falsk',
+    'historic_mode_file1': 'debug_empty_dataframe_returned',
+    'historic_mode_file2': 'plot_past_5min_10min_and_30min_data',
+    'historic_mode_file3': 'display_on_flask',
+    'historic_mode_file4': 'phase_2_spike_graph_with_sine_and_cosine'
+}
+
+print(todo_list)
